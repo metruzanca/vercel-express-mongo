fdc5a84e1b20d7e5088d7ef6c49ea7d8d28df509
Sam Zanca <sam@zanca.dev>
2023-02-24 16:16:31 -0500

    added missing dotenv import

--- api/seed.js
+++ api/seed.js
@@ -1,11 +1,12 @@
-import mongoose from 'mongoose'
+// import 'dotenv/config'
+// import mongoose from 'mongoose'
 
-// Leave these 4 lines
-//@ts-ignore
-mongoose.connect(process.env.DATABASE_URL)
-await seed()
-await mongoose.disconnect()
+// // Leave these 4 lines
+// //@ts-ignore
+// mongoose.connect(process.env.DATABASE_URL)
+// await seed()
+// await mongoose.disconnect()
 
-async function seed() {
-  // Seed the database here
-}
+// async function seed() {
+//   // Seed the database here
+// }
